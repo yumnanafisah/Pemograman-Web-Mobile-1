76c63f2ce76da39e11157aa5fba70b43eb4704b8
yumnanafisah <81919269+yumnanafisah@users.noreply.github.com>
2021-04-06 04:51:03 +0700

    Add files via upload

---
+++ pemogramanwebmobilemodul1.php
@@ -0,0 +1,36 @@
+<?php foreach ($anggota_keluarga = ["Bapak", "Ibu", "Makhmum Qalbi", "Yumna Nafisah", "Ilyasa Abdurrakhim", "Zunaizah Zulfikar"] as $ang){ ?>
+<br>Nama: <?= $ang; ?>
+<br>Jumlah Kata: <?= str_word_count($ang) ?>
+<br>Jumlah Huruf: <?= strlen($ang) ?>
+<br>Kebalikan Nama: <?= strrev($ang) ?> <br>
+
+<?php
+$teks_awal = trim($ang);
+$teks_modif = strtolower($teks_awal);
+$teks=str_replace(' ', '', $teks_modif);
+$jumlah = strlen($teks);
+
+$a = substr_count($teks, "a");
+$i = substr_count($teks, "i");
+$u = substr_count($teks, "u");
+$e = substr_count($teks, "e");
+$o = substr_count($teks, "o");
+
+$vokal = $a+$i+$u+$e+$o;
+$konsonan = $jumlah-$vokal;
+
+echo "Jumlah Karakter = $jumlah"."<br>";
+
+echo "Jumlah Huruf Vokal = $vokal"."<br>";
+
+echo "Jumlah Huruf Konsonan = $konsonan"."<br>";
+
+?>
+
+<br>
+<?php }
+
+?>
+
+
+
